7fe502ce9ea70bf8bfb059e4a500f857751deb4a
kavithakrish1 <kavi2910k@gmail.com>
2020-11-12 11:22:32 +0530

    first commit

---
+++ course/includes/menubar.php
@@ -0,0 +1,21 @@
+<section class="menu-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-0">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+
+                            <li><a href="session.php">Session</a></li>
+                              <li><a href="course.php">Course</a></li>
+                              <li><a href="student-registration.php">Registration</a></li>
+                               <li><a href="manage-students.php">Manage Students</a></li>
+                               <li><a href="enroll-history.php">Enroll History</a></li>
+                               
+                         
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
